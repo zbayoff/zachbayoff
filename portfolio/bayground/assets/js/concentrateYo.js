5193703676fd406f0c96b4969e6f73e823d1091f
Zach Bayoff <zachbayoff@Zachs-MacBook-Pro.local>
2018-04-18 07:27:09 -0400

    added images for concentration game, added open and close classes, and starting clicking event

--- portfolio/bayground/assets/js/concentrateYo.js
+++ portfolio/bayground/assets/js/concentrateYo.js
@@ -9,8 +9,12 @@
         if (gridSizeInput.value) {
             createGrid(parseInt(gridSizeInput.value));
         }
-    })
+    });
 
+    const gridContainer = document.querySelector('.project-concentrateYo__grid-container');
+
+    gridContainer.addEventListener('click', checkTile);
+    
     function createGrid(gridSize) {
 
         if (gridSize) {
@@ -24,24 +28,27 @@
             let gridMarkup = `
             ${
                 imgArrayShuffled.map((item) => {
-                    let tile = `<p><img src="/">${item}</p>`
+                    let tile = `<div><p class="imgTile closed"></p><img src="assets/img/concentrateYo/${item}.png"></div>`
                     return tile;
                 }).join('')
             }
         `;
-
-            const gridContainer = document.querySelector('.project-concentrateYo__grid-container');
             gridContainer.innerHTML = gridMarkup;
         }
 
     }
 
     function getImages(gridSize) {
+
+        let imgSrc = [];
         let imageArray = [];
 
-        for (let i = 0; i < (gridSize * gridSize); i += 1) {
-            imageArray.push(i);
+        for (let i = 0; i < (gridSize * gridSize) / 2; i += 1) {
+
+            imageArray.push(`tileImg-${i}`);
         }
+
+        imageArray.push(...imageArray);
 
         return imageArray;
     }
@@ -67,13 +74,37 @@
         return array;
     }
 
+    function checkTile(e) {
+
+        let clickCount = 0;
+
+        if ((e.target).classList.contains('imgTile')) {
+            console.log("you clicked a tile");
+            console.log(e.target);
+
+            let imgTile = e.target;
+
+            if (imgTile.classList.contains('closed')) {
+                imgTile.classList.add('open');
+                imgTile.classList.remove('closed');
+                clickCount += 1;
+                
+            }
+
+
+
+        }
+
+        e.stopPropagation();
+    }
+
     function startTimer() {}
 
     function stopTimer() {}
 
     function resetTimer() {}
 
-    function checkCard() {}
+
 
 
 })();
