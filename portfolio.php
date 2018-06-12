8e6fc7499b5b26f5408a410e3e4a5ae410dadb12
Zach Bayoff <zrbayoff@gmail.com>
2018-06-12 07:59:20 -0400

    minor styling changes, adding portfolio items

--- portfolio.php
+++ portfolio.php
@@ -55,19 +55,19 @@
                             <a class="container-project-desc__project-link" href="portfolio/roomies.php"><img class="container-project-desc__project-img" src="portfolio/roomies/assets/img/screenshots/dashboard-page.png"><h4 class="container-project-desc__project-title">Roomies: A Roommate Management App</h4></a>
                         </div>
                         <div class="container-project-desc__project-item">
-                            <a class="container-project-desc__project-link" href="portfolio/thebayground.php"><img class="container-project-desc__project-img" src="portfolio/bayground/assets/img/screenshots/thebayground-screenshot.png"><h4 class="container-project-desc__project-title">The Bayground: A Collection of Mini Apps</h4></a>
+                            <a class="container-project-desc__project-link" href="portfolio/thebayground.php"><img class="container-project-desc__project-img" src="assets/images/project-desc-bayground.png"><h4 class="container-project-desc__project-title">The Bayground: A Collection of Mini Apps</h4></a>
                         </div>
                         <div class="container-project-desc__project-item">
-                            <a class="container-project-desc__project-link" href="/"><img class="container-project-desc__project-img" src="portfolio/bayground/assets/img/screenshots/thebayground-screenshot.png"><h4 class="container-project-desc__project-title">zachbayoff.com</h4></a>
+                            <a class="container-project-desc__project-link" href="/"><img class="container-project-desc__project-img" src="assets/images/project-desc-zachbayoff.png"><h4 class="container-project-desc__project-title">zachbayoff.com</h4></a>
                         </div>
                         <div class="container-project-desc__project-item">
-                            <a class="container-project-desc__project-link" href="/"><img class="container-project-desc__project-img" src="portfolio/bayground/assets/img/screenshots/thebayground-screenshot.png"><h4 class="container-project-desc__project-title">Weather App</h4></a>
+                            <a class="container-project-desc__project-link" href="/"><img class="container-project-desc__project-img" src="assets/images/project-desc-weatherapp.png"><h4 class="container-project-desc__project-title">Weather App</h4></a>
+                        </div>
+                        <div class="container-project-desc__project-item">
+                            <a class="container-project-desc__project-link" href="portfolio/travel-app.php"><img class="container-project-desc__project-img" src="assets/images/project-desc-travelapp.png"><h4 class="container-project-desc__project-title">Travel App</h4></a>
                         </div>
                         <div class="container-project-desc__project-item">
                             <a class="container-project-desc__project-link" href="/"><img class="container-project-desc__project-img" src="portfolio/bayground/assets/img/screenshots/thebayground-screenshot.png"><h4 class="container-project-desc__project-title">NYJTL Mockup</h4></a>
-                        </div>
-                        <div class="container-project-desc__project-item">
-                            <a class="container-project-desc__project-link" href="/"><img class="container-project-desc__project-img" src="portfolio/bayground/assets/img/screenshots/thebayground-screenshot.png"><h4 class="container-project-desc__project-title">Travel App</h4></a>
                         </div>
                     </div
                 </div>
---
+++ portfolio/travel-app.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zach Bayoff | Web Developer | Project Description: Travel App</title>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Alef|Reem+Kufi" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115178608-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-115178608-1');
+
+    </script>
+    
+</head>
+
+<body id="portfolio-travelapp">
+    <header class="header">
+        <div class="logo">
+            <p class="logo__text"><a href="../index.php">ZachBayoff</a></p>
+        </div>
+        <nav>
+            <ul class="nav">
+                <li class="nav__nav-item"><a class="nav__nav-link" href="../index.php">About</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="../portfolio.php">Portfolio</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="../contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="page">
+    <main class="container">
+        <section class="row">
+            <div>
+            <nav class="breadcrumbs">
+                    <ul>
+                        <li>
+                            <a href="../portfolio.php">Portfolio</a>
+                        </li>
+                        <span>/</span>
+                        <li>
+                            <a href="travel-app.php">Travel App - Description</a>
+                        </li>
+                    </ul>
+                </nav>
+                <h1>Project Name: Travel App</h1>
+                <p>Client: Anybody</p>
+                <p>Tags: Angular 6 (CLI) Single Page Application / Heroku App / Material.io / REST API / CRUD Application / MondoDB</p>
+                <h2>Project Background</h2>
+                <p>This web application was created for those who like to travel, but may not have an easy way to keep track of all of their desired destinations and associated expenses. With Travel App (pardon the lack of inticing app name), a user can create, update, and delete a 'trip', filling in details such as the trip destination, travel dates, and list of costs one might incur while traveling.</p>
+                <p>This app also calls the Unsplash API that allows the user to find high resolution, beautiful photographs of just about anything, and could maybe help remind them of the beauties that their destinations hold.</p>
+                <p>A MongoDB database was set up to store the users trips, meaning that anyone can access anyone's trips. This was implemented more on a proof-of-concept basis than an actual user authentication app. I may add this implementation in the future. </p>
+                <h2>Link to application</h2>
+                <button class="container-project-background__project-btn"><a class ="container-project-background__project-link" href="https://zb-travel-app.herokuapp.com/" target="_blank">Launch Application</a></button>
+            </div>
+         </section>
+ 
+        <section class="row">
+            <div class="column project-desc-container">
+                <h3>Sign Up</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/signup-page.png" class="container-project-screenshot__project-img">
+                </div>
+                <h3>Login</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/login-page.png" class="container-project-screenshot__project-img">
+                </div>
+
+                <h3>Groups</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/groups-page.png" class="container-project-screenshot__project-img">
+                </div>
+                <h3>Dashboard</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/dashboard-page.png" class="container-project-screenshot__project-img">
+                </div>
+                <h3>Chores</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/chores-page.png" class="container-project-screenshot__project-img">
+                </div>
+                <h3>Items</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/items-page.png" class="container-project-screenshot__project-img">
+                </div>
+                <h3>Visitors</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/visitors-page.png" class="container-project-screenshot__project-img">
+                </div>
+                <h3>Preferences</h3>
+                <div class="container-project-screenshot__project-item">
+                    <img src="roomies/assets/img/screenshots/preferences-page.png" class="container-project-screenshot__project-img">
+                </div>
+            </div>
+        </section>
+  
+    </main>
+
+
+    <?php include '../assets/partials/footer.php' ?>
+    </div>
+    
+    <script src="../assets/js/scripts.js"></script>
+
+</body>
+
+</html>
